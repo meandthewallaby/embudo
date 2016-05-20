fd56788400e14f8a189439453f1390298def13a5
Eric Hewitt <eric@understoryweather.com>
2016-05-19 22:43:55 -0400

    Fixed private function calling

--- accel.py
+++ accel.py
@@ -12,35 +12,35 @@ class Accelerometer:
         self.pitch = 0
         self.roll = 0
 
-    def _pack(val1, val2):
+    def _pack(self, val1, val2):
         a = struct.unpack('>h', chr(val1)+chr(val2))[0]
         return a
 
-    def _calc_accel_val(val1, val2):
-        return _pack(val1, val2) / 16384.
+    def _calc_accel_val(self, val1, val2):
+        return self._pack(val1, val2) / 16384.
 
-    def _calc_gyro_val(val1, val2):
-        return _pack(val1, val2) / 65.5
+    def _calc_gyro_val(self, val1, val2):
+        return self._pack(val1, val2) / 65.5
 
-    def _read_vals():
+    def _read_vals(self):
         self.i2c.write_i2c_block_data(ACCEL_ADDR, 0x00, [])
         raw_vals = self.i2c.read_i2c_block_data(ACCEL_ADDR, 0x3B)
         ret = {"accel": {"x": None, "y": None, "z": None}, "gyro": {"x": None, "y": None, "z": None}}
-        ret["accel"]["x"] = _calc_accel_val(raw_vals[0], raw_vals[1])
-        ret["accel"]["y"] = _calc_accel_val(raw_vals[2], raw_vals[3])
-        ret["accel"]["z"] = _calc_accel_val(raw_vals[4], raw_vals[5])
-        ret["gyro"]["x"] = _calc_gyro_val(raw_vals[8], raw_vals[9])
-        ret["gyro"]["y"] = _calc_gyro_val(raw_vals[10], raw_vals[11])
-        ret["gyro"]["z"] = _calc_gyro_val(raw_vals[12], raw_vals[13])
+        ret["accel"]["x"] = self._calc_accel_val(raw_vals[0], raw_vals[1])
+        ret["accel"]["y"] = self._calc_accel_val(raw_vals[2], raw_vals[3])
+        ret["accel"]["z"] = self._calc_accel_val(raw_vals[4], raw_vals[5])
+        ret["gyro"]["x"] = self._calc_gyro_val(raw_vals[8], raw_vals[9])
+        ret["gyro"]["y"] = self._calc_gyro_val(raw_vals[10], raw_vals[11])
+        ret["gyro"]["z"] = self._calc_gyro_val(raw_vals[12], raw_vals[13])
         return ret
 
-    def _distance(a, b):
+    def _distance(self, a, b):
         return math.sqrt((a*a)+(b*b))
 
     def calc_pitch_and_roll(self):
-        a = _read_vals()
-        p = math.degrees(math.atan2(a["accel"]["y"], _distance(a["accel"]["x"], a["accel"]["z"])))
-        r = math.degrees(math.atan2(a["acce"]["x"], distance(a["accel"]["y"], a["accel"]["z"])))
+        a = self._read_vals()
+        p = math.degrees(math.atan2(a["accel"]["y"], self._distance(a["accel"]["x"], a["accel"]["z"])))
+        r = math.degrees(math.atan2(a["acce"]["x"], self._distance(a["accel"]["y"], a["accel"]["z"])))
 
         sample_period = 1.
 
